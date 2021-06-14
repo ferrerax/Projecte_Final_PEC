ae20e5cd46335f5884df8dd029e773dd8ee1dafd
xXIty <miquel.roset@upc.edu>
2021-06-14 20:14:51 +0200

    fix process to unify registers

--- sd_driver/sd_driver.vhd
+++ sd_driver/sd_driver.vhd
@@ -19,19 +19,14 @@ signal byte_counter : std_logic_vector(8 downto 0) := (others => '0');
 signal offset : std_logic_vector(8 downto 0) := (others => '0');
 begin
 	offset <= addr(8 downto 1) & '0'; -- 2 byte align addr%512
-	process (rd, addr, dout_avail, busy) begin
-		if rising_edge(rd) then -- clear valid for each new read op
-				valid <= '0';
-		end if;
-		if ( rd = '1' )  then	
-			if ( rising_edge(busy) ) then -- init counter when op start
-				byte_counter <= (others => '0');
-			end if;
-		end if;
-		if ( dout_avail = '0' ) then -- init counter when op start
-				dout_taken <= '0';
-		end if;
-		if ( busy = '1' ) then -- during read operation
+	dout_taken <= dout_avail;
+	process (dout_avail, busy) begin
+		
+      if (busy = '0' ) then
+		 valid <= '0';
+		 byte_counter <= (others => '0');
+
+		else  -- during read operation
 			if ( rising_edge(dout_avail) ) then -- new byte readed
 				if ( offset = byte_counter(8 downto 1) & '0' ) then
 					if ( byte_counter(0) = '1' ) then
@@ -41,7 +36,7 @@ begin
 						data(7 downto 0) <= dout;
 					end if;
 				end if;
-				dout_taken <= '1';
+				-- dout_taken <= '1';
 				byte_counter <= byte_counter + 1;
 
 			end if;
